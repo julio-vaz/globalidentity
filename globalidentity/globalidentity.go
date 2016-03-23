e0826f22d45ab0fe3bbfe559aa56d655d36f94a7
Julio Vaz <jvaz@stone.com.br>
2016-03-23 17:55:33 -0300

    Integracao basica com o global identity

--- globalidentity/globalidentity.go
+++ globalidentity/globalidentity.go
@@ -3,23 +3,90 @@ package Globalidentity
 import (
 	"github.com/levigross/grequests"
 	"fmt"
-	"encoding/json"
+	"crypto/sha512"
+	"crypto/hmac"
+	"encoding/hex"
 )
 
-type GlobalIdentityResponse struct{
-	args string
-	headers map[string] string
-	origin string
-	url string
-}
+var Api = "https://dlpgi.dlp-payments.com/api/"
+
 
 func GlobalIdentity() bool{
-	resp, err := grequests.Get("http://httpbin.org/get", nil)
-	var respMap GlobalIdentityResponse{}
-	if err != nil{
-		fmt.Println("Deu merda")
-	}
-	json.Unmarshal([]byte(resp.String()), &respMap)
-	fmt.Println(respMap)
+	resp, _ := grequests.Get("http://httpbin.org/get", nil)
+	var tes map[string]interface{}
+	fmt.Println(tes)
+	resp.JSON(&tes)
+	fmt.Println(tes["origin"])
 	return true
+}
+
+
+func AuthenticateUser(applicationKey string, email string, password string, expirationInMinutes int) map[string]interface{} {
+	json := map[string]interface{}{
+		"Email": email,
+		"Password": password,
+		"ApplicationKey": applicationKey,
+		"TokenExpirationInMinutes": expirationInMinutes,
+	}
+	resp, _ := grequests.Post(Api + "Authorization/authenticate", &grequests.RequestOptions{
+        JSON: json,
+    })
+	var response map[string]interface{}	
+	resp.JSON(&response)
+
+	return response
+}
+
+func ValidateToken(applicationKey string, token string) map[string]interface{} {
+	json := map[string]interface{}{
+		"ApplicationKey": applicationKey,
+		"Token": token,
+	}
+	resp, _ := grequests.Post(Api + "Authorization/ValidateToken", &grequests.RequestOptions{
+		JSON: json,
+	})
+	var response map[string]interface{}
+	resp.JSON(&response)
+
+	return response
+}
+
+func HasRoles(applicationKey string, userKey string, roles []string) map[string]interface{} {
+	json := map[string]interface{}{
+		"ApplicationKey": applicationKey,
+		"UserKey": userKey,
+		"RoleCollection": roles,
+	}
+
+	resp, _ := grequests.Post(Api + "Authorization/IsUserInRole", &grequests.RequestOptions{
+		JSON: json,
+	})
+	var response map[string]interface{}
+	resp.JSON(&response)
+
+	return response
+}
+
+func ValidateApplication(applicationKey string, clientApplicationKey string, clientSecretKey string, resources string) map[string]interface{} {
+	hmac512 := hmac.New(sha512.New, []byte(clientSecretKey))
+	hmac512.Write([]byte(resources))
+	encryptedSecretKey := hex.EncodeToString(hmac512.Sum(nil))
+	fmt.Println(encryptedSecretKey)
+
+	json := map[string]interface{}{
+		"ApplicationKey": applicationKey,
+		"ClientApplicationKey": clientApplicationKey,
+		"RawData": resources,
+		"EncryptedData": encryptedSecretKey,
+	}
+
+	fmt.Println(json)
+
+	resp, _ := grequests.Post(Api + "Authorization/ValidateApplication", &grequests.RequestOptions{
+		JSON: json,
+	})
+	var response map[string]interface{}
+	resp.JSON(&response)
+
+	return response
 }
--- main.go
+++ main.go
@@ -5,7 +5,22 @@ import(
 	"fmt"
 )
 
-func main(){
-	a := GlobalIdentity()
+func main(){ 
+	applicationKey := "302c257e-984c-4773-940a-fd2eb3ba030a"
+	expirationInMinutes := 65
+	email := "scar0000@gmail.com"
+	password := "stone2016"
+	a := AuthenticateUser(applicationKey, email, password, expirationInMinutes)
 	fmt.Println(a)
+
+	token := "CE2A9D089704BC379A1E0528AB1454EA49B55122FA1911257DF85EC4747B9AA8863229F3DAC324AD0AC16D1C14FAF06C481C69F91734B916FE10443933A736198E376B856591899F67644B5D618A0AA4"
+	b := ValidateToken(applicationKey, token)
+	fmt.Println(b)
+
+
+	clientApplicationKey := "3154c332-d3ab-4f4f-9d2f-e181858dd72d"
+	clientSecretKey := "A3D3E8E1048B2E5F9FFD50A815910FD6447B8A31"
+	resources := "teste3"
+	c := ValidateApplication(applicationKey, clientApplicationKey, clientSecretKey, resources)
+	fmt.Println(c)
 }
